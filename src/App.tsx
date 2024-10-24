f7774d5a873e8a64107cd613a69f18d6bb0758f0
jihui <mzhujihui@163.com>
2024-10-24 14:22:10 +0800

    feat: add tailwindcss and lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import './App.css';
+import { Button, Input } from "@douyinfe/semi-ui";
 
 const App = () => {
-  return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-purple-400">Rsbuild with React</h1>
+			<p>Start building amazing things with Rsbuild.</p>
+			<Button theme="light" type="primary">
+				浅色主要
+			</Button>
+			<Input defaultValue="hi" />
+		</div>
+	);
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import "./style/index.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById("root");
 if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+	const root = ReactDOM.createRoot(rootEl);
+	root.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>,
+	);
 }
